ea700134bc790d4f777bb98dad7a049be89fc283
Denzkristangacusan <gdenzkristan@gmail.com>
2024-11-04 17:45:39 +0800

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+
+
+// Collect user information
+let fullName = "Denz kristan"; 
+let age = 20; 
+let favoriteNumber = 8; 
+let favoriteColor = "Blue"; 
+
+// Log 
+console.log("User Profile:");
+console.log("Full Name:", fullName);
+console.log("Age:", age);
+console.log("Favorite Number:", favoriteNumber);
+console.log("Favorite Color:", favoriteColor);
